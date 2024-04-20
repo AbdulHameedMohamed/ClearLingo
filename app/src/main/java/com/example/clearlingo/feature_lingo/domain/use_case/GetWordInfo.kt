3dec7c79135a2b38623006c8544a43d13941116a
AbdulHameed Mohamed <59577985+AbdulHameedMohamed@users.noreply.github.com>
2024-04-20 18:33:02 +0200

    Setup Domain Layer.

---
+++ app/src/main/java/com/example/clearlingo/feature_lingo/domain/use_case/GetWordInfo.kt
@@ -0,0 +1,18 @@
+package com.example.clearlingo.feature_lingo.domain.use_case
+
+import com.example.clearlingo.feature_lingo.core.util.Resource
+import com.example.clearlingo.feature_lingo.domain.model.WordInfo
+import com.example.clearlingo.feature_lingo.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetWordInfo(
+    private val repository: WordInfoRepository
+) {
+    operator fun invoke(word: String): Flow<Resource<List<WordInfo>>> {
+        if(word.isBlank()) {
+            return flow {  }
+        }
+        return repository.getWordInfo(word)
+    }
+}
